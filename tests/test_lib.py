8ca3b3aa283c613ecc9687593c82b0a3ca55ddf3
AN <devnull@localhost>
2014-09-10 23:54:47 +0200

    Unittest for shell escaping

---
+++ tests/test_lib.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import unittest
+import sys
+sys.path.append('.')
+
+import pashlib
+
+
+def B(s):
+    " Avoid quoting backslashes all the time "
+    return s.replace('B', '\\').replace('S', "'").replace('D', '"')
+
+class Test(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(B('B S DB'), '\\ \' "\\')
+
+    def test_escape_sh(self):
+        data = [
+            ('nothing', 'nothing'),
+            ('with spaces', 'withB spaces'),
+            ('with Bs', 'withB BBs'),
+            ('keep DquotesD and SquotesS', 'keepB DquotesDB andB SquotesS'),
+            ('with BDs', 'withB BBDs'),
+            ('', ''),
+        ]
+        for raw, escaped in data:
+            self.assertEqual(
+                pashlib.escape_sh(B(raw)),
+                B(escaped),
+            )
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test)
+    unittest.TextTestRunner(verbosity=2).run(suite)
